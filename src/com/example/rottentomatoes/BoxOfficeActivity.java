86400d9afda2601691631af8d0db5c44ca703fcb
Karun Siddana <ksiddana@gmail.com>
2015-02-08 09:08:19 -0800

    CodePath Workshop
    Rotten Tomatoes Application built during CodePath Workshops.

---
+++ src/com/example/rottentomatoes/BoxOfficeActivity.java
@@ -0,0 +1,19 @@
+package com.example.rottentomatoes;
+
+import com.example.simpletodolist.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+public class BoxOfficeActivity extends Activity {
+ 	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_box_office);
+		
+		}
+		
+} // end of OnCreate method
+
---
+++ src/com/example/rottentomatoes/override.java
@@ -0,0 +1,5 @@
+package com.example.rottentomatoes;
+
+public @interface override {
+
+}
